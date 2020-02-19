b32c6afb37c80e2997f1f2d03a6e868a9f20ee03
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-02-18 22:39:04 -0500

    restructured the conditions

--- COMP212_MidtermPracticeTest_CompositeControl/Form1.cs
+++ COMP212_MidtermPracticeTest_CompositeControl/Form1.cs
@@ -16,7 +16,8 @@ namespace COMP212_MidtermPracticeTest_CompositeControl
         public string name;
         public int age;
         bool convertingAgetoInteger;
-        
+
+
         public Form1()
         {
             InitializeComponent();
@@ -26,33 +27,43 @@ namespace COMP212_MidtermPracticeTest_CompositeControl
         {
             name = txtName.Text;
             convertingAgetoInteger = int.TryParse(txtAge.Text, out age);
-
-            if (userCompositeControl1.Male.Checked==true)
+           
+            if (userCompositeControl1.Male.Checked == true)
             {
-             if (age==18 && age<=25)
+                if (age >= 18 && age <= 25)
                 {
-                    MessageBox.Show("Hi " + name + "You are " + age + " years old " + userCompositeControl1.Male.Text + " and therefore, your discount rate is 20%");
+                    MessageBox.Show("Hi " + name + " You are " + age + " years old " + userCompositeControl1.Male.Text + " and therefore, your discount rate is 20%");
                 }
-             else if (age >=26)
+                else if (age >= 26)
                 {
-                    MessageBox.Show("Hi " + name + "You are  " + age + " years old " + userCompositeControl1.Male.Text + " and therefore, your discount rate is 22% ");
+                    MessageBox.Show("Hi " + name + " You are  " + age + " years old " + userCompositeControl1.Male.Text + " and therefore, your discount rate is 22% ");
+                }
+                else
+                {
+                    MessageBox.Show("Sorry, You are not eligible for any discount yet !", "Important Query", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 }
             
             }
-            
-            else if(userCompositeControl1.Female.Checked==true)
-            {
-                if (age == 18 && age <= 25)
-                {
-                    MessageBox.Show("Hi " + name + "You are " + age + " years old " + userCompositeControl1.Female.Text + " and therefore, your discount rate is 20%");
-                }
-               else if(age>=26)
-                {
-                    MessageBox.Show("Hi " + name + "You are " + age + " years old " + userCompositeControl1.Female.Text + " and therefore, your discount rate is 25%");
-                }
-             }
-           
 
+            else if (userCompositeControl1.Female.Checked == true)
+            {
+                if (age >= 18 && age <= 25)
+                {
+                    MessageBox.Show("Hi " + name + " You are " + age + " years old " + userCompositeControl1.Female.Text + " and therefore, your discount rate is 20%");
+                }
+                else if (age >= 26)
+                {
+                    MessageBox.Show("Hi " + name + " You are " + age + " years old " + userCompositeControl1.Female.Text + " and therefore, your discount rate is 25%");
+                }
+                else
+                {
+                    MessageBox.Show("Sorry, You are not eligible for any discount yet !","Important Query", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+
+            }
+           
+                   
         }
-    }
+
+     }
 }
