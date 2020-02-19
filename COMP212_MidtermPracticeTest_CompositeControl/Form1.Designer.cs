c6d533d79033c5b4d4370981d6a7b0c7f44ca0fb
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-02-18 20:19:56 -0500

    created composite control with radio buttons and added to form

--- COMP212_MidtermPracticeTest_CompositeControl/Form1.Designer.cs
+++ COMP212_MidtermPracticeTest_CompositeControl/Form1.Designer.cs
@@ -28,13 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.userCompositeControl1 = new COMP212_MidtermPracticeTest_CompositeControl.UserCompositeControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnDiscount = new System.Windows.Forms.Button();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // userCompositeControl1
+            // 
+            this.userCompositeControl1.Location = new System.Drawing.Point(50, 12);
+            this.userCompositeControl1.Name = "userCompositeControl1";
+            this.userCompositeControl1.Size = new System.Drawing.Size(104, 121);
+            this.userCompositeControl1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Age:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(113, 167);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(147, 20);
+            this.txtName.TabIndex = 3;
+            // 
+            // btnDiscount
+            // 
+            this.btnDiscount.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDiscount.Location = new System.Drawing.Point(64, 251);
+            this.btnDiscount.Name = "btnDiscount";
+            this.btnDiscount.Size = new System.Drawing.Size(115, 31);
+            this.btnDiscount.TabIndex = 4;
+            this.btnDiscount.Text = "Discount Rate";
+            this.btnDiscount.UseVisualStyleBackColor = true;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(113, 206);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(147, 20);
+            this.txtAge.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(444, 336);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.btnDiscount);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userCompositeControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private UserCompositeControl userCompositeControl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnDiscount;
+        private System.Windows.Forms.TextBox txtAge;
     }
 }
 
---
+++ COMP212_MidtermPracticeTest_CompositeControl/UserCompositeControl.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace COMP212_MidtermPracticeTest_CompositeControl
+{
+    partial class UserCompositeControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rdbtnMale = new System.Windows.Forms.RadioButton();
+            this.rdbtnFemale = new System.Windows.Forms.RadioButton();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rdbtnMale
+            // 
+            this.rdbtnMale.AutoSize = true;
+            this.rdbtnMale.Location = new System.Drawing.Point(9, 9);
+            this.rdbtnMale.Name = "rdbtnMale";
+            this.rdbtnMale.Size = new System.Drawing.Size(48, 17);
+            this.rdbtnMale.TabIndex = 0;
+            this.rdbtnMale.TabStop = true;
+            this.rdbtnMale.Text = "Male";
+            this.rdbtnMale.UseVisualStyleBackColor = true;
+            // 
+            // rdbtnFemale
+            // 
+            this.rdbtnFemale.AutoSize = true;
+            this.rdbtnFemale.Location = new System.Drawing.Point(9, 42);
+            this.rdbtnFemale.Name = "rdbtnFemale";
+            this.rdbtnFemale.Size = new System.Drawing.Size(59, 17);
+            this.rdbtnFemale.TabIndex = 1;
+            this.rdbtnFemale.TabStop = true;
+            this.rdbtnFemale.Text = "Female";
+            this.rdbtnFemale.UseVisualStyleBackColor = true;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(9, 84);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(91, 32);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // UserCompositeControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.rdbtnFemale);
+            this.Controls.Add(this.rdbtnMale);
+            this.Name = "UserCompositeControl";
+            this.Size = new System.Drawing.Size(104, 121);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rdbtnMale;
+        private System.Windows.Forms.RadioButton rdbtnFemale;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
